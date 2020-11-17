38c42c96bde7735b66586bad905aec1bcb47cb26
Soumen Saha <65912583+SoumenSaha7@users.noreply.github.com>
2020-11-17 19:41:18 +0530

    Add files via upload

---
+++ OnlineAttendanceSystem.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package onlineattendancesystem;
+
+// import java.util.Scanner;
+
+/**
+ *
+ * @author SOUMEN
+ */
+public class OnlineAttendanceSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new password().setVisible(true);
+    }
+    
+}
